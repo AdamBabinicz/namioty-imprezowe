1d5b0fc8cb43e2b00f0e00ca6ad906471ce9f70c
babinicz <babinicz@onet.pl>
2024-07-20 16:21:17 +0200

    dodałem linki

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { Tooltip } from "react-tooltip";
-import { FaFacebook, FaTwitter, FaPinterest } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaPinterest,
+  FaExternalLinkAlt,
+} from "react-icons/fa";
 
 const FooterContainer = styled.footer`
   display: flex;
@@ -41,6 +46,38 @@ const SocialIcons = styled.div`
       font-size: 1.2rem;
       margin: 0 1.25rem; /* Zmniejsza marginesy w wersji mobilnej */
     }
+  }
+`;
+
+const ExternalLinks = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    font-size: 1rem;
+    color: ${({ theme }) => theme.text};
+    margin: 0 1rem; /* Dodałem margines po bokach */
+    transition: color 0.3s ease, transform 0.3s ease;
+
+    &:hover {
+      color: ${({ theme }) => theme.linkHover};
+      transform: scale(1.1);
+    }
+
+    @media (max-width: 768px) {
+      font-size: 0.9rem;
+      margin: 0.5rem 0; /* Marginesy góra-dół w wersji mobilnej */
+    }
+  }
+
+  span {
+    margin-left: 0.5rem;
   }
 `;
 
@@ -106,12 +143,61 @@ export const Footer = () => {
           </motion.div>
         </a>
       </SocialIcons>
+      <ExternalLinks>
+        <a
+          href="https://www.slodkolandia.cba.pl"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-tooltip-id="slodkolandiaTooltip"
+          data-tooltip-content="Słodkolandia wita"
+        >
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.3 }}
+          >
+            <FaExternalLinkAlt />
+            <span>Słodkolandia wita</span>
+          </motion.div>
+        </a>
+        <a
+          href="https://zamki.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-tooltip-id="zamkiTooltip"
+          data-tooltip-content="Zamki"
+        >
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.3 }}
+          >
+            <FaExternalLinkAlt />
+            <span>Zamki</span>
+          </motion.div>
+        </a>
+        <a
+          href="https://www.fontanny.slodkolandia.cba.pl"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-tooltip-id="fontannyTooltip"
+          data-tooltip-content="Fontanny"
+        >
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.3 }}
+          >
+            <FaExternalLinkAlt />
+            <span>Fontanny</span>
+          </motion.div>
+        </a>
+      </ExternalLinks>
       <FooterText>Radom, 2024 - {new Date().getFullYear()}.</FooterText>
 
-      <Tooltip id="tentTooltip" />
       <Tooltip id="facebookTooltip" />
       <Tooltip id="twitterTooltip" />
       <Tooltip id="pinterestTooltip" />
+      <Tooltip id="slodkolandiaTooltip" />
+      <Tooltip id="zamkiTooltip" />
+      <Tooltip id="fontannyTooltip" />
     </FooterContainer>
   );
 };
