712d03871b7794acdab3a2d726ebba223e22a662
babinicz <babinicz@onet.pl>
2024-07-20 15:17:01 +0200

    zmiana zdjęcia

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,7 +7,7 @@ import image1 from "../assets/1.jpg";
 import image2 from "../assets/2.jpg";
 import image3 from "../assets/3.jpg";
 import image4 from "../assets/19.jpg";
-import image5 from "../assets/2.jfif";
+import image5 from "../assets/4.png";
 import image6 from "../assets/18.jpg";
 import { lightTheme, darkTheme } from "../styles/themes";
 import "slick-carousel/slick/slick.css";
